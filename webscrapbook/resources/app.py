f3969ec454c836a238797a75c6981ce27fcd5edb
Danny Lin <danny0838@gmail.com>
2020-04-12 23:17:38 +0800

    Optimize server and app code.

--- webscrapbook/resources/app.py
+++ webscrapbook/resources/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 from webscrapbook.app import make_app
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root = os.path.abspath(os.path.join(__file__, '..', '..'))
 application = make_app(root)
--- webscrapbook/resources/serve.py
+++ webscrapbook/resources/serve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 from webscrapbook import server
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root = os.path.abspath(os.path.join(__file__, '..', '..'))
 server.serve(root)
