06793798f01fd7efe7d3db47f4bde965adb468e4
Max Nikulin <manikulin@gmail.com>
2020-09-18 10:51:16 +0700

    Make assert_file_equal() failures a bit more clear

--- test/test_app_actions.py
+++ test/test_app_actions.py
@@ -169,10 +169,11 @@ class TestActions(unittest.TestCase):
                         }
 
             for i in stat0:
-                if i in {'mtime', 'atime'}:
-                    self.assertAlmostEqual(stat0[i], stati[i], delta=2)
-                else:
-                    self.assertEqual(stat0[i], stati[i])
+                with self.subTest(i=i):
+                    if i in {'mtime', 'atime'}:
+                        self.assertAlmostEqual(stat0[i], stati[i], delta=2)
+                    else:
+                        self.assertEqual(stat0[i], stati[i])
 
     def parse_sse(self, content):
         """Quick parser of SSE.
