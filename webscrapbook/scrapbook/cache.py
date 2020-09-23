14d1b20b6fe4a4f459541fc8ce0df9bceac64d29
Danny Lin <danny0838@gmail.com>
2020-09-23 12:20:53 +0800

    Improve error handling and debug information

--- webscrapbook/scrapbook/cache.py
+++ webscrapbook/scrapbook/cache.py
@@ -59,6 +59,7 @@ class FulltextCacheGenerator():
         # 'parsererror',
         'svg', 'math',
         }
+    URL_SAMPLE_LENGTH = 256
 
     def __init__(self, book, *, inclusive_frames=True):
         self.book = book
@@ -109,23 +110,27 @@ class FulltextCacheGenerator():
                 os.utime(file)
 
     def _cache_item(self, id):
+        yield Info('debug', f'Checking item "{id}"')
         book = self.book
 
         # remove id if no meta
         meta = book.meta.get(id)
         if meta is None:
+            yield Info('debug', f'Purging item "{id}" (missing metadata)')
             yield from self._delete_item(id)
             return
 
         # remove id if no index
         index = meta.get('index')
         if not index:
+            yield Info('debug', f'Purging item "{id}" (no index)')
             yield from self._delete_item(id)
             return
 
         # remove id if no index file
         indexfile = os.path.join(book.data_dir, index)
         if not os.path.exists(indexfile):
+            yield Info('debug', f'Purging item "{id}" (missing index file)')
             yield from self._delete_item(id)
             return
 
@@ -136,7 +141,7 @@ class FulltextCacheGenerator():
         files_to_update = MutatingDict()
 
         item = FulltextCacheItem(id, meta, index, indexfile, files_to_update)
-        self._collect_files_to_update(item)
+        yield from self._collect_files_to_update(item)
         yield from self._handle_files_to_update(item)
 
     def _delete_item(self, id):
@@ -156,14 +161,17 @@ class FulltextCacheGenerator():
             # than cache file, for better performance
             if util.is_archive(indexfile):
                 if os.stat(indexfile).st_mtime <= self.cache_last_modified:
+                    yield Info('debug', f'Skipped "{id}" (archive file older than cache)')
                     return
 
         # add index file(s) to update list
         for path in book.get_index_paths(index):
+            yield Info('debug', f'Adding "{path}" of "{id}" to check list (from index)')
             files_to_update[path] = True
 
         # add files in cache to update list
         for path in book.fulltext[id]:
+            yield Info('debug', f'Adding "{path}" of "{id}" to check list (from cache)')
             files_to_update[path] = True
 
     def _handle_files_to_update(self, item):
@@ -182,8 +190,10 @@ class FulltextCacheGenerator():
         has_update = False
 
         for path in files_to_update:
+            yield Info('debug', f'Checking "{path}" of "{id}"')
             # remove from cache if marked False
             if not files_to_update[path]:
+                yield Info('debug', f'Purging "{path}" of "{id}" (inlined)')
                 if path in book.fulltext[id]:
                     yield from report_update()
                     del book.fulltext[id][path]
@@ -192,9 +202,10 @@ class FulltextCacheGenerator():
             # mark False to prevent added otherwhere
             files_to_update[path] = False
 
-            mtime = self._get_mtime(item, path)
+            mtime = yield from self._get_mtime(item, path)
             if mtime is None:
                 # path not exist => delete from cache
+                yield Info('debug', f'Purging "{path}" of "{id}" (file not exist)')
                 if path in book.fulltext[id]:
                     yield from report_update()
                     del book.fulltext[id][path]
@@ -205,17 +216,19 @@ class FulltextCacheGenerator():
             #   updated file, and thus needs update even if it's mtime is not
             #   newer.
             if path in book.fulltext[id] and mtime <= self.cache_last_modified:
+                yield Info('debug', f'Skipped "{path}" of "{id}" (file older than cache)')
                 continue
 
             yield from report_update()
 
             # set updated fulltext
+            yield Info('debug', f'Generating cache for "{path}" of "{id}"')
             try:
-                fulltext = self._get_fulltext_cache(item, path)
+                fulltext = yield from self._get_fulltext_cache(item, path)
             except Exception as exc:
                 fulltext = ''
                 traceback.print_exc()
-                yield Info('error', f'Failed to generate cache for "{id}" ({path})', exc=exc)
+                yield Info('error', f'Failed to generate cache for "{id}" ({path}): {exc}', exc=exc)
 
             if fulltext is not None:
                 book.fulltext[id][path] = {
@@ -235,11 +248,17 @@ class FulltextCacheGenerator():
                     return util.zip_timestamp(info)
                 except KeyError:
                     return None
+                except Exception as exc:
+                    yield Info('error', f'Failed to access in-zip-file for "{path}" of "{item.id}": {exc}', exc=exc)
+                    return None
 
         file = os.path.join(self.book.data_dir, os.path.dirname(item.index), path)
         try:
             return os.stat(file).st_mtime
+        except (FileNotFoundError, IsADirectoryError, NotADirectoryError):
+            return None
         except OSError:
+            yield Info('error', f'Failed to access file for "{path}" of "{item.id}": [Errno {exc.args[0]}] {exc.args[1]}', exc=exc)
             return None
 
     def _open_file(self, item, path):
@@ -249,36 +268,44 @@ class FulltextCacheGenerator():
                     return zh.open(path)
                 except KeyError:
                     return None
+                except Exception as exc:
+                    yield Info('error', f'Failed to open in-zip-file for "{path}" of "{item.id}": {exc}', exc=exc)
+                    return None
 
         file = os.path.join(self.book.data_dir, os.path.dirname(item.index), path)
         try:
             return open(file, 'rb')
-        except OSError:
-            # @TODO: show error message for exist but unreadable file?
+        except (FileNotFoundError, IsADirectoryError, NotADirectoryError):
+            return None
+        except OSError as exc:
+            yield Info('error', f'Failed to open file for "{path}" of "{item.id}": [Errno {exc.args[0]}] {exc.args[1]}', exc=exc)
             return None
 
     def _get_fulltext_cache(self, item, path):
-        fh = self._open_file(item, path)
+        fh = yield from self._open_file(item, path)
         if not fh:
+            yield Info('debug', f'Skipped "{path}" of "{item.id}" (file not exist or accessible)')
             return None
 
         fh = zip_stream(fh)
         try:
             mime, _ = mimetypes.guess_type(path)
-            return self._get_fulltext_cache_for_fh(item, path, fh, mime)
+            return (yield from self._get_fulltext_cache_for_fh(item, path, fh, mime))
         finally:
             fh.close()
 
     def _get_fulltext_cache_for_fh(self, item, path, fh, mime):
         if not mime:
+            yield Info('debug', f'Skipped "{path}" of "{item.id}" (unknown type)')
             return None
 
         if util.mime_is_html(mime):
-            return self._get_fulltext_cache_html(item, path, fh)
+            return (yield from self._get_fulltext_cache_html(item, path, fh))
 
         if mime.startswith('text/'):
-            return self._get_fulltext_cache_txt(item, fh)
+            return (yield from self._get_fulltext_cache_txt(item, path, fh))
 
+        yield Info('debug', f'Skipped "{path}" of "{item.id}" ("{mime}" not supported)')
         return None
 
     def _get_fulltext_cache_html(self, item, path, fh):
@@ -317,16 +344,18 @@ class FulltextCacheGenerator():
 
             return target
 
-        # @TODO: show message for malformed data URLs
         def add_datauri_content(url):
             try:
                 data = util.parse_datauri(url)
-            except util.DataUriMalformedError:
+            except util.DataUriMalformedError as exc:
+                yield Info('error', f'Skipped malformed data URL "{url[:self.URL_SAMPLE_LENGTH]}": {exc}', exc=exc)
                 return
             fh = io.BytesIO(data.bytes)
-            fulltext = self._get_fulltext_cache_for_fh(item, None, fh, data.mime)
+            fulltext = yield from self._get_fulltext_cache_for_fh(item, None, fh, data.mime)
             if fulltext:
                 results.append(fulltext)
+
+        yield Info('debug', f'Retrieving HTML content for "{path}" of "{item.id}"')
 
         # Seek for the correct charset (encoding).
         # If a charset is not specified, lxml may select a wrong encoding for
@@ -357,10 +386,11 @@ class FulltextCacheGenerator():
                 continue
 
             if url.startswith('data:'):
-                add_datauri_content(url)
+                yield from add_datauri_content(url)
             else:
                 target = get_relative_file_path(url)
                 if target and target not in item.files_to_update:
+                    yield Info('debug', f'Adding "{target}" of "{item.id}" to check list (from <meta>)')
                     item.files_to_update[target] = True
 
         # Add data URL content of meta refresh targets to fulltext index if the
@@ -404,10 +434,11 @@ class FulltextCacheGenerator():
                         pass
                     else:
                         if url.startswith('data:'):
-                            add_datauri_content(url)
+                            yield from add_datauri_content(url)
                         else:
                             target = get_relative_file_path(url)
                             if target and target not in item.files_to_update:
+                                yield Info('debug', f'Adding "{target}" of "{item.id}" to check list (from <{elem.tag}>)')
                                 item.files_to_update[target] = True
 
                 elif elem.tag in ('iframe', 'frame'):
@@ -418,7 +449,7 @@ class FulltextCacheGenerator():
                         pass
                     else:
                         if url.startswith('data:'):
-                            add_datauri_content(url)
+                            yield from add_datauri_content(url)
                         else:
                             target = get_relative_file_path(url)
                             if target:
@@ -427,12 +458,14 @@ class FulltextCacheGenerator():
                                     # content if the targeted file hasn't
                                     # been indexed.
                                     if item.files_to_update.get(target) is not False:
+                                        yield Info('debug', f'Caching "{target}" of "{item.id}" as inline (from <{elem.tag}>)')
                                         item.files_to_update[target] = False
-                                        fulltext = self._get_fulltext_cache(item, target)
+                                        fulltext = yield from self._get_fulltext_cache(item, target)
                                         if fulltext:
                                             results.append(fulltext)
                                 else:
                                     if target not in item.files_to_update:
+                                        yield Info('debug', f'Adding "{target}" of "{item.id}" to check list (from <{elem.tag}>)')
                                         item.files_to_update[target] = True
 
                 # exclude everything inside certain tags
@@ -479,7 +512,8 @@ class FulltextCacheGenerator():
 
         return self.FULLTEXT_SPACE_REPLACER(' '.join(results)).strip()
 
-    def _get_fulltext_cache_txt(self, item, fh):
+    def _get_fulltext_cache_txt(self, item, path, fh):
+        yield Info('debug', f'Retrieving text content for "{path}" of "{item.id}"')
         charset = util.sniff_bom(fh) or item.meta.get('charset') or 'UTF-8'
         charset = util.fix_codec(charset)
         text = fh.read().decode(charset, errors='replace')
