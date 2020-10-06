eb951bb4f87d4eb43c098a332ea1dff8fb725407
Danny Lin <danny0838@gmail.com>
2020-10-06 22:39:27 +0800

    Fix test failure of locales

--- test/test_locales.py
+++ test/test_locales.py
@@ -9,12 +9,12 @@ class TestI18N(unittest.TestCase):
         self.assertEqual(i18n.langs, ['en'])
 
         i18n = I18N('en_GB')
-        self.assertEqual(i18n.lang, 'en_GB')
-        self.assertEqual(i18n.langs, ['en_GB', 'en'])
+        self.assertEqual(i18n.lang, 'en_gb')
+        self.assertEqual(i18n.langs, ['en_gb', 'en'])
 
         i18n = I18N('zh_TW')
-        self.assertEqual(i18n.lang, 'zh_TW')
-        self.assertEqual(i18n.langs, ['zh_TW', 'zh', 'en'])
+        self.assertEqual(i18n.lang, 'zh_tw')
+        self.assertEqual(i18n.langs, ['zh_tw', 'zh', 'en'])
 
     def test_call01(self):
         i18n = I18N('zh_TW')
@@ -22,7 +22,7 @@ class TestI18N(unittest.TestCase):
 
     def test_special(self):
         i18n = I18N('zh_TW')
-        self.assertEqual(i18n('@@ui_locale'), 'zh_TW')
+        self.assertEqual(i18n('@@ui_locale'), 'zh_tw')
         self.assertEqual(i18n('@@bidi_dir'), 'ltr')
         self.assertEqual(i18n('@@bidi_reversed_dir'), 'rtl')
         self.assertEqual(i18n('@@bidi_start_edge'), 'left')
