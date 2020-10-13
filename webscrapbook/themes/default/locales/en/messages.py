02f7fd825d7358ccc63f2a70b8824e309a7f12a5
Danny Lin <danny0838@gmail.com>
2020-10-13 22:17:19 +0800

    Add syntax help for search template

--- webscrapbook/themes/default/locales/en/messages.py
+++ webscrapbook/themes/default/locales/en/messages.py
@@ -14,6 +14,28 @@ cache_index_source_link_title = 'Source link'
 cache_search_title = '%book% :: Search'
 cache_search_view_in_map = 'View in map'
 cache_search_start = 'go'
+cache_search_help_label = 'Search syntax help'
+cache_search_help_desc = """\
+• Input keywords search from title, content, or comment.
+• Use space to separate multiple keywords. For example, “w3c organization” means items containing “w3c” and “organization”.
+• Use double quotes to demark a complete phrase, a literal double quote can be escaped by doubling. For example, “"Tom ""loves"" Mary."” means items containing “Tom "loves" Mary.”
+• Use minus sign to exclude a keyword. For example, “-virus” means items without “virus”.
+• Use “<command>:<keyword>” to specify a special search condition. A minus sign can be prefixed for exclusion or reversion. Available commands include:
+  • mc: each subsequent keyword matches case-sensitively. For example, “mc: CIA FBI -mc: president” means items containing “CIA” and “FBI” case-sensitively, and “president” case-insensitively.
+  • re: each subsequent keyword is treated as a regular expression. For example, “re: \\bcolou?r\\b -re: 1+1=2” means items that match regular expression “\\bcolou?r\\b” and contain keyword “1+1=2”.
+  • id: item whose ID equal to the keyword (or match the regular expression). Multiple values are “or”-connected. For example, “id:2020 id:2021” means items of ID “2020” or “2021”; “-id:2020” means items whose ID is not “2020”.
+  • type: items whose type equal to the keyword (or match the regular expression). Multiple values are “or”-connected. Available types are “” (page), “bookmark”, “file”, “note”, etc. For example, “type: type:bookmark” means items whose type is page or bookmark.
+  • title: items whose title contains the keyword.
+  • content: items whose fulltext contains the keyword.
+  • comment: items whose comment contains the keyword.
+  • tcc: items whose title, fulltext, or comment contains the keyword.
+  • file: items whose filename contains the keyword.
+  • root: items under the item of ID. Multiple values are “or”-connected.
+  • book: items in the specific scrapbook (by name). Multiple values are “or”-connected.
+  • source: items whose source URL contains the keyword.
+  • create: items whose create time matches the condition. Time format is an interval with 0-17 digits, followed by a minus sign optionally, and then followed by 0-17 digits. Each omitted digit is assumed to be a "0", except that "999..." is used if the latter date is totally omitted. For example, “create:2014-2015” means 20140000000000000 to 20150000000000000; “create:-201309” means 00000000000000000 to 20130900000000000; and “create:20110825” means 20110825000000000 to 99999999999999999.
+  • modify: items whose modify time matches the condition. Time format is same as create.
+  • sort: sort search results using the specific condition, which can be id, title, comment, content, source, type, create, or modify. For example, “sort:id -sort:modify” means sorting by ID in acending order and then sorting by modify time in descending order."""
 cache_search_result = 'Found %length% results:'
 cache_search_result_named = '(%name%) Found %length% results:'
 cache_search_sort_last_created = 'Last Created'
--- webscrapbook/themes/default/locales/zh/messages.py
+++ webscrapbook/themes/default/locales/zh/messages.py
@@ -14,6 +14,28 @@ cache_index_source_link_title = '來源連結'
 cache_search_title = '%book% :: 搜尋'
 cache_search_view_in_map = '在索引頁中檢視'
 cache_search_start = '查'
+cache_search_help_label = '搜尋語法說明'
+cache_search_help_desc = """\
+• 輸入的關鍵詞會在標題、內文、評註中查找。
+• 可用半形空白分隔要檢索的多個關鍵詞。例如「w3c organization」表示搜尋含有「w3c」及「organization」的項目 。
+• 可用半形雙引號檢索完整關鍵詞，若關鍵詞包含半形雙引號，可用連續兩個半形雙引號表示。例如「"Tom ""loves"" Mary."」表示搜尋含有「Tom "loves" Mary.」的項目。
+• 可用負號排除關鍵詞。例如「-virus」表示搜尋不含「virus」的項目。
+• 可用「<命令>:<關鍵詞>」指定特殊的檢索條件，命令前可加負號表示排除或反向。可用命令如下：
+  • mc：之後的關鍵詞皆比對大小寫。加負號則反之。例如「mc: CIA FBI -mc: president」表示搜尋含有區分大小寫的「CIA」、「FBI」及不分大小寫的「president」的項目。
+  • re：之後的關鍵詞皆視為正規表示式。加負號則反之。例如「re: \\bcolou?r\\b -re: 1+1=2」表示搜尋匹配正規表示式「\\bcolou?r\\b」且含有關鍵詞「1+1=2」的項目。
+  • id：搜尋 ID 等同關鍵詞（或與正規表示式匹配）的項目。多次指定時以「或」連接。例如「id:2020 id:2021」表示搜尋 ID 為「2020」或「2021」的項目；「-id:2020」表示搜尋 ID 不為「2020」的項目。
+  • type：搜尋類型等同關鍵詞（或與正規表示式匹配）的項目。多次指定時以「或」連接。可用的類型有「」（網頁）、「bookmark」（書籤）、「file」（檔案）、「note」（筆記）等等。例如「type: type:bookmark」表示搜尋類型為網頁或書籤的項目。
+  • title：搜尋標題含有關鍵詞的項目。
+  • content：搜尋全文含有關鍵詞的項目。
+  • comment：搜尋評註含有關鍵詞的項目。
+  • tcc：搜尋標題、全文或評註含有關鍵詞的項目。
+  • file：搜尋檔名含有關鍵詞的項目。
+  • root：搜尋此 ID 項目下的子項目。多次指定時以「或」連接。
+  • book：搜尋指定剪貼簿（依名稱）中的項目。多次指定時以「或」連接。
+  • source：搜尋原始網址含有關鍵詞的項目。
+  • create：搜尋建立時間符合條件的項目。時間格式為 0-17 位數字，後面接負號（可略），再接 0-17 位數字。省略的部分自動補 0，但後面的時間全省略時補 9（即無限大）。例如「create:2014-2015」表示 20140000000000000 到 20150000000000000，「create:-201309」表示 00000000000000000 到 20130900000000000，「create:20110825」表示 20110825000000000 到 99999999999999999。
+  • modify：搜尋修改時間符合條件的項目。時間格式同 create。
+  • sort：將搜尋結果按指定方式排序，負號表示倒序。可填入 id、title、comment、content、source、type、create、modify。例如「sort:id -sort:modify」表示先按 ID 遞增排序再按修改時間遞減排序。"""
 cache_search_result = '找到 %length% 筆結果：'
 cache_search_result_named = '(%name%) 找到 %length% 筆結果：'
 cache_search_sort_last_created = '最後建立'
