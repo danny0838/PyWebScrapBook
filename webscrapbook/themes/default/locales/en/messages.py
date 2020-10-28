0e3d31a9b76283ba72427c915da1ee9125c10883
Danny Lin <danny0838@gmail.com>
2020-10-28 11:09:08 +0800

    Revise search doc

--- webscrapbook/themes/default/locales/en/messages.py
+++ webscrapbook/themes/default/locales/en/messages.py
@@ -29,15 +29,16 @@ cache_search_help_desc = """\
   • content: items whose fulltext contains the keyword.
   • comment: items whose comment contains the keyword.
   • tcc: items whose title, fulltext, or comment contains the keyword.
-  • file: items whose filename contains the keyword.
-  • root: items under the item of ID. Multiple values are “or”-connected.
-  • book: items in the specific scrapbook (by name). Multiple values are “or”-connected.
+  • index: items whose index file path contains the keyword.
   • source: items whose source URL contains the keyword.
   • icon: items whose icon URL contains the keyword.
-  • create: items whose create time matches the condition. Time format is an interval with 0-17 digits, followed by a minus sign optionally, and then followed by 0-17 digits. Each omitted digit is assumed to be a "0", except that "999..." is used if the latter date is totally omitted. For example, “create:2014-2015” means 20140000000000000 to 20150000000000000; “create:-201309” means 00000000000000000 to 20130900000000000; and “create:20110825” means 20110825000000000 to 99999999999999999.
+  • create: items whose create time matches the condition. The time condition is an interval with 0-17 digits, followed by a minus sign optionally, and then followed by 0-17 digits. The two 17-digit numbers means the year (4 digits), month (01-12), day (01-31), hours (00-59), minutes (00-59), seconds (00-59), and milliseconds (000-999) in local datetime. Each omitted digit is assumed to be a “0”, except that “999...” is assumed if the end datetime is totally omitted. For example, “create:2014-2015” means since 2014 until 2015; “create:-201309” means before Sep 2013; and “create:20110825” means after Aug 25, 2011.
   • modify: items whose modify time matches the condition. Time format is same as create.
   • marked: marked items.
   • locked: locked items.
+  • file: items whose filename contains the keyword.
+  • root: items under the item of ID. Multiple values are “or”-connected.
+  • book: items in the specific scrapbook (by name). Multiple values are “or”-connected.
   • sort: sort search results using the specific condition, which can be id, title, comment, content, source, type, create, or modify. For example, “sort:id -sort:modify” means sorting by ID in acending order and then sorting by modify time in descending order."""
 cache_search_result = 'Found %length% results:'
 cache_search_result_named = '(%name%) Found %length% results:'
