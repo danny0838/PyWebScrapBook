97d3593f00a84fa1eedfa7e127d5149ff2d9d5c2
Danny Lin <danny0838@gmail.com>
2021-11-04 17:02:22 +0800

    Release 0.46.0

--- webscrapbook/__init__.py
+++ webscrapbook/__init__.py
@@ -11,7 +11,7 @@ import re
 __all__ = ['WSB_EXTENSION_MIN_VERSION', 'WSB_USER_DIR', 'WSB_USER_CONFIG', 'WSB_DIR', 'WSB_CONFIG', 'config']
 
 __package_name__ = 'webscrapbook'
-__version__ = '0.45.1'
+__version__ = '0.46.0'
 __author__ = 'Danny Lin'
 __author_email__ = 'danny0838@gmail.com'
 __homepage__ = 'https://github.com/danny0838/PyWebScrapBook'
