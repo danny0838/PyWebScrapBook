db235fe8fe2ede9a6f9395cf4d7e24028b742445
Danny Lin <danny0838@gmail.com>
2022-08-28 01:05:37 +0800

    Use single quotes for setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from inspect import cleandoc
 import webscrapbook
 
-with open("README.md", encoding="utf-8") as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -12,23 +12,23 @@ setup(
     version=webscrapbook.__version__,
     description=cleandoc(webscrapbook.__doc__),
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     author=webscrapbook.__author__,
     author_email=webscrapbook.__author_email__,
     url=webscrapbook.__homepage__,
     license=webscrapbook.__license__,
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: Flask",
-        "Natural Language :: English",
-        "Topic :: Database",
-        "Topic :: Internet",
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Flask',
+        'Natural Language :: English',
+        'Topic :: Database',
+        'Topic :: Internet',
         ],
     python_requires='~=3.6',
     install_requires=[
@@ -39,7 +39,7 @@ setup(
         'commonmark >= 0.8',
         ],
     extras_require={
-        "adhoc_ssl": ["cryptography"],
+        'adhoc_ssl': ['cryptography'],
         },
     packages=find_packages(exclude=['test']),
     package_data={
