17161b9911fd24f758677119f02cc18803f202e6
Danny Lin <danny0838@gmail.com>
2024-01-18 09:34:30 +0800

    Add compile script

---
+++ tools/compile.cmd
@@ -0,0 +1,39 @@
+:: Compile portable executable for Windows (x86/x64).
+:: Add --onefile to compile as single executable (less performant).
+::
+:: NOTE: webscrapbook package should be installed in non-editable mode.
+::
+@echo off
+set "dir=%~dp0.."
+set "build=%dir%\build"
+set "dist=%dir%\dist"
+set "res=%dir%\tools\compile"
+if "%~1"=="--onefile" (
+  set "onefile=--onefile"
+) else (
+  set "onefile="
+)
+
+:: Check if the module has been installed
+pip show webscrapbook >nul 2>nul
+if %errorlevel% neq 0 (
+  echo ERROR: webscrapbook is not installed
+  exit /b %errorlevel%
+)
+
+python -m pip install --upgrade pyinstaller
+
+:: Clean the build folder
+if exist "%build%" rmdir /s /q "%build%"
+
+pyinstaller "%res%\wsb.py" %onefile% --noconfirm ^
+  --workpath "%build%" --specpath "%build%" --distpath "%dist%" -n wsb ^
+  --hidden-import webscrapbook.cli ^
+  --add-data "%dir%\webscrapbook\resources:webscrapbook\resources" ^
+  --add-data "%dir%\webscrapbook\themes:webscrapbook\themes"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if "%onefile%" == "" (
+  python -m zipfile -c "%dist%\wsb.zip" "%dist%\wsb"
+  rmdir "%dist%\wsb" /s /q
+)
---
+++ tools/compile/wsb.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import webscrapbook.cli
+webscrapbook.cli.main()
