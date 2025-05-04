092419c9db54b8452934c0cfbe520c54f53c56cf
Danny Lin <danny0838@gmail.com>
2025-05-04 22:59:01 +0800

    ci: improve builder script and release workflow
    - Allow selecting environment.
    - Reduce unneeded artifacts handling.
    - Run PyPI publishing and executables building parallelly.
    - Move permissions to top-level scope.
    - Support building on more platforms.
    - Rewrite builder scripts using Python.

---
+++ tools/build.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""Compile executable.
+
+NOTE: This package must be installed in non-editable mode.
+"""
+import argparse
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+import PyInstaller.__main__
+
+from webscrapbook import __version__
+
+root = os.path.abspath(os.path.join(__file__, '..', '..'))
+
+
+def detect_os_arch():
+    os_ = {
+        'Windows': 'win',
+        'Linux': 'linux',
+        'Darwin': 'mac',
+    }.get(platform.system(), 'unknown')
+
+    arch = 'x64' if (sys.maxsize > 2 ** 32) else 'x86'
+
+    if os_ == 'mac':
+        if arch == 'x64' and platform.machine() in ('arm64', 'aarch64'):
+            arch = 'arm64'
+        else:
+            raise RuntimeError(f'Unknown Python architecture under {platform.system()} {platform.machine()}')
+
+    return os_, arch
+
+
+def build_binary(args):
+    build = os.path.join(root, 'build')
+    dist = os.path.join(root, 'dist')
+    assets = os.path.join(root, 'assets')
+
+    # determine dist dir according to system information
+    version = __version__
+    pyver = f'{sys.version_info.major}.{sys.version_info.minor}'
+    os_, arch = detect_os_arch()
+    onefile = '-onefile' if args.onefile else ''
+
+    pack_name = f'webscrapbook-{version}-py{pyver}-{os_}-{arch}{onefile}'
+    dist = os.path.join(dist, pack_name)
+
+    # run the compiler
+    pyinstaller_args = [
+        os.path.join(assets, 'wsb.py'),
+        '--name', 'wsb',
+        '--workpath', build,
+        '--specpath', build,
+        '--distpath', dist,
+        '--hidden-import', 'webscrapbook.cli',
+        '--add-data', ':'.join((os.path.join(root, 'webscrapbook', 'resources'), os.path.join('webscrapbook', 'resources'))),
+        '--add-data', ':'.join((os.path.join(root, 'webscrapbook', 'themes'), os.path.join('webscrapbook', 'themes'))),
+        '--icon', os.path.join(assets, 'icon32.ico'),
+        *(['--onefile'] if args.onefile else []),
+        '--noconfirm',
+        '--clean',
+    ]
+
+    os.chdir(root)
+    PyInstaller.__main__.run(pyinstaller_args)
+
+    # pack as ZIP
+    if args.pack:
+        pack_root = dist if args.onefile else os.path.join(dist, 'wsb')
+        shutil.make_archive(dist, 'zip', pack_root)
+        shutil.rmtree(dist)
+
+
+def upload(args):
+    build = os.path.join(root, 'build')
+    dist = os.path.join(root, 'dist')
+
+    # Purge previously built files by bdist (wheel) to prevent deleted files
+    # being included in the package.
+    if os.path.isdir(build):
+        shutil.rmtree(build)
+
+    subprocess.run([sys.executable, '-m', 'build', '--sdist', '--wheel', root])
+    subprocess.run([sys.executable, '-m', 'twine', 'upload', '--skip-existing', os.path.join(dist, '*')])
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description='Build executable with PyInstaller')
+    parser.add_argument('--onefile', action='store_true', help='Bundle the app into a single executable')
+    parser.add_argument('--pack', action='store_true', help='Pack into a zip file')
+    parser.add_argument('--upload', action='store_true', help='Build and upload to PyPI')
+    return parser.parse_args(argv)
+
+
+def main():
+    args = parse_args()
+
+    if args.upload:
+        upload(args)
+    else:
+        build_binary(args)
+
+
+if __name__ == '__main__':
+    main()
